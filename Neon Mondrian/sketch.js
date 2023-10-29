cdc2e7fc99ad5306d5ca32b9e07f77adbbbba9d5
Yuanqing <yl11249@nyu.edu>
2023-10-29 00:37:56 -0400

    detect color

--- Neon Mondrian/sketch.js
+++ Neon Mondrian/sketch.js
@@ -1,106 +1,76 @@
-let img1;
-let img2;
-let img3;
-let img4; 
-let currentImage;
+let img;
 let t = 0;
-let slider;
-let switchButton1;
-let switchButton2; 
-let switchButton3; 
-let switchButton4; 
 
 function preload() {
-  img1 = loadImage("./Mondriaan.jpg");
-  img2 = loadImage("./M2.jpg");
-  img3 = loadImage("./M3.jpg");
-  img4 = loadImage("./M4.jpg");
-  currentImage = img1; 
-}
-
-function switchToImage1() {
-  currentImage = img1;
-  updateImage();
-}
-
-function switchToImage2() {
-  currentImage = img2;
-  updateImage();
-}
-
-function switchToImage3() {
-  currentImage = img3;
-  updateImage();
-}
-
-function switchToImage4() {
-  currentImage = img4;
-  updateImage();
-}
-
-function updateImage() {
-  let aspectRatio = currentImage.width / currentImage.height;
-  let newHeight = windowHeight;
-  let newWidth = windowHeight * aspectRatio;
-  currentImage.resize(newWidth, newHeight);
-  currentImage.loadPixels();
+  img = loadImage("./M1.jpg");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  slider = createSlider(0, 255, 0);
-  slider.position(width - 200, 100);
-
-  switchButton1 = createButton('Image 1');
-  switchButton1.position(width - 200, 150);
-  switchButton1.mousePressed(switchToImage1);
-
-  switchButton2 = createButton('Image 2');
-  switchButton2.position(width - 200, 180);
-  switchButton2.mousePressed(switchToImage2);
-
-  switchButton3 = createButton('Image 3');
-  switchButton3.position(width - 200, 210);
-  switchButton3.mousePressed(switchToImage3);
-
-  switchButton4 = createButton('Image 4');
-  switchButton4.position(width - 200, 240);
-  switchButton4.mousePressed(switchToImage4);
-
   pixelDensity(1);
-  updateImage();
+  print("Original size: ", img.width, " x ", img.height);
+
+  let aspectRatio = img.width / img.height;
+  let newHeight = windowHeight;
+  let newWidth = windowHeight * aspectRatio;
+  img.resize(newWidth, newHeight);
+  print("Scaled size: ", img.width, " x ", img.height);
+
+  img.loadPixels();
+  print("pixel array size: ", img.pixels.length); 
+
+  
 }
 
 function draw() {
   background(10, 10);
-  noStroke();
+  noStroke(); 
 
-  for (let y = 0; y < currentImage.height; y += 30) {
-    for (let x = 0; x < currentImage.width; x += 30) {
-      let pIndex = 4 * (y * currentImage.width + x);
-      let currentVal = slider.value();
-      let redValue = currentImage.pixels[pIndex + 0 + currentVal];
-      let greenValue = currentImage.pixels[pIndex + 1];
-      let blueValue = currentImage.pixels[pIndex + 2 + currentVal];
+  let xOffset = (width - img.width) / 2;
+  let yOffset = (height - img.height) / 2; 
 
-      const xAngle = map(mouseX, 0, width, -4 * PI, 4 * PI, true);
-      const yAngle = map(mouseY, 0, height, -4 * PI, 4 * PI, true);
-      const angle = xAngle * (x / width) + yAngle * (y / height);
-
-      const myX = x + 20 * cos(2 * PI * t + angle);
-      const myY = y + 20 * sin(2 * PI * t + angle);
-
-      fill(redValue, greenValue, blueValue);
-      ellipse(myX, myY, 10);
+  for (let vi = 0; vi < img.pixels.length; vi += 4) {
+    let redVal = img.pixels[vi];
+    let greenVal = img.pixels[vi + 1];
+    let blueVal = img.pixels[vi + 2]; 
+    if (redVal > greenVal * 2 && redVal > blueVal * 2) {
+    
+      img.pixels[vi + 3] = 0;
     }
   }
 
-  t = t + 0.01;
+  img.updatePixels(); 
 
-}
+  image(img, xOffset, yOffset);
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-  updateImage();
+  for (let y = 0; y < img.height; y += 20) {
+    for (let x = 0; x < img.width; x += 20) {
+      let index = (y * img.width + x) * 4;
+      let redVal = img.pixels[index];
+
+      if (
+        redVal > img.pixels[index + 1] * 2 &&
+        redVal > img.pixels[index + 2] * 2 
+      ) {
+        
+        const angle =
+          map(mouseX, 0, width, -4 * PI, 4 * PI, true) * (x / width) +
+          map(mouseY, 0, height, -4 * PI, 4 * PI, true) * (y / height);
+
+        const myX = x + 20 * cos(2 * PI * t + angle) + xOffset;
+        const myY = y + 20 * sin(2 * PI * t + angle) + yOffset; 
+
+        fill(
+          img.pixels[index],
+          img.pixels[index + 1],
+          img.pixels[index + 2],
+          255
+        ); 
+        ellipse(myX, myY, 10);
+      }
+    }
+  }
+
+  t = t + 0.01; 
 }
--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-# p5.js multi sketch template
+### Neon Mondrian  
 
-By putting different sketch.js and index.html files in directories we can more easily switch between multiple experiments.
+*1. How are you detecting the colors?*  
 
+
+*2. What are you changing them to?*  
+I add the [Wavemaker](https://p5js.org/examples/interaction-wavemaker.html) effect to the painting.
+
+*3. What happens if you run your sketch on images of different Mondrian paintings?*  
+The effect applys to the different paintings. I created 4 buttons for users to switch different paintings.
